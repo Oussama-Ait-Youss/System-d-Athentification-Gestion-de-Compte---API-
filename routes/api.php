436cce9ee634229b0295fd598e8995ef3f734b77
Oussama-Ait-Youss <alikhay815@gmail.com>
2026-03-11 13:55:48 +0100

    feat: configure the routes in the api.php

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,18 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProfileController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    
+    Route::get('/me', [ProfileController::class, 'me']);
+    Route::put('/me', [ProfileController::class, 'update']);
+    Route::put('/me/password', [ProfileController::class, 'updatePassword']);
+    Route::delete('/me', [ProfileController::class, 'destroy']);
+});
+
